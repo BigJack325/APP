2d65bfc05fc829fa2bfaa83462306aefa586ec38
AMYK0901 <72214532+AMYK0901@users.noreply.github.com>
2020-10-06 12:54:41 -0400

    Add files via upload

--- sinus.c
+++ sinus.c
@@ -1,60 +1,88 @@
+/*
+Fichier: Sinus
+Auteurs: Kevin Amyot amyk0901
+         Olivier Jackson jaco2303
+Date: 05 octobre 2020
+Description: Calcul du sinus avec une serie
+*/
+// Description: Calcul du cosinus avec un angle en radians
+// Préconditions: Avoir une valeur x en radian entre 0 et 2π
+// Postconditions: Retourne une valeur entre 0 et 1
+
 #include <stdio.h>
-int signe(int n)
+#define iterations 5
+
+float puissance(float base, float exponent)
 {
-	int reponse1=1,i;
-	for(i=1; i<n; i++)
-	{
-		reponse1 = reponse1* (-1);
-	}
-	return reponse1;
+	float power=1;
+	int i=1;
+	
+	if(exponent==0)
+		{
+			return 1;
+		}	
+	while(i<= exponent)
+		{
+			power = base * power;
+			i=i+1;
+		}
+	return power;
 }
 
-float exponentielle( float x, int n)
+int factoriel(int factor)
 {
-	float reponse2=1,i;
-	for(i=1; i<=(2*n-1);i++)
+	int i=1;
+	int factvalu=1;
+	
+		for(i=1; i<=factor; i++)
 	{
-		reponse2 = reponse2*x;
+		factvalu= factvalu * i;
 	}
-	return reponse2;
+		return factvalu;
 }
+
+
+float calculsinus(float radian)
+{
+	float somme=0;
+	int counter;
 		
-int factorielle(int n)
-{
-	int reponse3=1,i;
-	for(i=1; i<=(2*n-1);i++)
-	{
-		reponse3 = reponse3*i;
-	}
-	return reponse3;
+		for(counter=1; counter<=iterations; counter++)
+		{
+			somme= (somme) + (( puissance((-1),counter-1) * puissance(radian, (2*counter-1))) / factoriel(2*counter-1));
+		}
+	return somme;
 }
-float sinus(float x)
-{
-	const int n=5;
-	float resultat=0;
-	for(int i=1; i<=n;i++) 
-	{
-		int reponse1 = signe(i);
-		float reponse2 = exponentielle(x,i);
-		int reponse3 = factorielle(i);
-		resultat = resultat+((reponse1*reponse2)/reponse3);
-	}
-	return resultat;
-}
-		
-int main()
-{
-	float x[4] = {1.0,0.0,(3.14/4),(3.14/2)};
-	float theorique[4] = {0.8415,0.0,0.7071,1.0};
-	for(int i=0;i<4;i++)
-	{
-		float resultat=sinus(x[i]);
-		printf("La valeur en radian testé est: %f\n",x[i]);
-		printf("La valeur du sinus théorique est: %f\n",theorique[i]);
-		printf("La valeur du sinus calculée est: %f\n",resultat);
-	}
 
 
+
+
+
+int main(int argc, char **argv)
+{
+	float radians= 1;
+	float reponse = calculsinus(radians);
+	printf("valeur donner: %f\n",radians);
+	printf("resultat voulu:0.8415\n");
+	printf("resultat %f\n\n", reponse);
+	
+	float radians2= 0;
+	float reponse2 = calculsinus(radians2);
+	printf("valeur donner: %f\n",radians2);
+	printf("resultat voulu:0\n");
+	printf("resultat %f\n\n", reponse2);
+	
+	float radians3= 3.14159/4;
+	float reponse3 = calculsinus(radians3);
+	printf("valeur donner: %f\n",radians3);
+	printf("resultat voulu:0.7071\n");
+	printf("resultat %f\n\n", reponse3);
+	
+	float radians4= 3.14159/2;
+	float reponse4 = calculsinus(radians4);
+	printf("valeur donner: %f\n",radians4);
+	printf("resultat voulu:1\n");
+	printf("resultat %f\n\n", reponse4);
+	
 	return 0;
 }
-
